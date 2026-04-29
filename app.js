3f282411cdf1216106bfb4681e0145ad6f1bded5
ChaurasiyaDharmendra <golu78dk@gmail.com>
2026-04-29 09:33:07 +0530

    fix: home route redirect

--- app.js
+++ app.js
@@ -85,7 +85,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public"))); 
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 // Routes
 app.use("/listings", listings);
